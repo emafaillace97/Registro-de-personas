3efa265bc925089aafb501aa5d7f801401e58177
emafaillace97 <emafaillace2018@gmail.com>
2024-11-17 23:37:31 -0300

    Se creo la clase Registor y se implemento el metodo agregar persona

---
+++ Registro.py
@@ -0,0 +1,9 @@
+class Registro:
+    
+    def __init__(self):
+        self.__personas = []
+    
+    def agregar_persona(self, persona):
+        self.__personas.append(persona)
+
+if __name__ == "__main__": pass
