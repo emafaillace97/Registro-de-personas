a2c6580e4b5ab76e64bef47e98e9b36d7a9e8110
emafaillace97 <emafaillace2018@gmail.com>
2024-11-17 22:27:45 -0300

    Se creo la clase Persona

---
+++ Persona.py
@@ -0,0 +1,10 @@
+class Persona:
+
+    def __init__(self, nombre, apellido):
+        self.__nombre = nombre
+        self.__apellido = apellido
+    
+    def __str__(self):
+        return f"Nombre: {self.__nombre} - Apellido: {self.___apellido}"
+
+if __name__ == "__main__": pass
