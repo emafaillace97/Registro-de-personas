f2e84fa5e7329c8f771d71f3b11ac7df7367e443
emafaillace97 <emafaillace2018@gmail.com>
2024-11-17 22:57:54 -0300

    Update Persona.py
    Se agrego el metodo get_nombre

--- Persona.py
+++ Persona.py
@@ -3,6 +3,9 @@ class Persona:
     def __init__(self, nombre, apellido):
         self.__nombre = nombre
         self.__apellido = apellido
+
+    def get_nombre(self):
+        return self.__nombre
     
     def __str__(self):
         return f"Nombre: {self.__nombre} - Apellido: {self.___apellido}"
