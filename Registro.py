d89f4768c1bd87f711bbb42ea6db1cdb2ba583da
emafaillace97 <emafaillace2018@gmail.com>
2024-11-17 23:55:01 -0300

    Se agrego el metodo de listas personas

--- Registro.py
+++ Registro.py
@@ -8,5 +8,9 @@ class Registro:
     
     def eliminar_persona(self, persona):
         self.__personas.remove(persona)
+    
+    def mostrar_personas(self):
+        for persona in self.__personas:
+            print(persona)
 
 if __name__ == "__main__": pass
